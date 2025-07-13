f7cdcc8311abc3b296e3f793c0345bfc5e15cd1d
CoreClientIO <nonnull.coreclient@gmail.com>
2025-07-13 18:44:03 +0200

    Fixed setup.sh

--- install_tpm.sh
+++ install_tpm.sh
@@ -2,6 +2,4 @@
 
 wget https://github.com/IcyHenryT/TPM-Loader/releases/latest/download/TPM-loader-linux && chmod +x TPM-loader-linux
 
-echo "Please configure TPM now."
-
-./TPM-loader-linux
+echo "Please configure TPM after the script is done. (Start using ./TPM-loader-linux)"
