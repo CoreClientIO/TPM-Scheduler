9415194457dcc945017092841bdcd2b71be75b82
CoreClientIO <nonnull.coreclient@gmail.com>
2025-07-13 18:35:46 +0200

    Added setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo "Executing copy script"
-
+echo "Fixing copy_files permission"
 chmod +x ./copy_files.sh
+
+echo "Executing copy script"
 ./copy_files.sh
 
 echo "Executing build"
 pyinstaller tpm-scheduler.spec
+
+echo "Moving dist/tpm-scheduler to current directory"
+mv dist/tpm-scheduler .
+
+echo "Fixing tpm-scheduler permission"
+chmod +x tpm-scheduler
+
+echo "Completed. You can now start the app using ./tpm-scheduler"
